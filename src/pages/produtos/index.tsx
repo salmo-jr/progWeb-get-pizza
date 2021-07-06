3c7c27a326f0c87a7366288d869c0074906600f5
Salmo Marques <salmo.sjunior@sp.senac.br>
2021-07-06 19:30:59 -0300

    commit inicial

---
+++ src/pages/produtos/index.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+interface IProduct {
+    Title: string;
+    Year: string;
+    imdbID: string;
+    Type: string;
+    Poster: string;
+}
+
+const Produtos = () => {
+    const [products, setProducts] = useState<IProduct[]>([]);
+
+    const loadingProducts = async () => {
+        const results = await api.get(`/?apikey=925eba28&s=batman`);
+        setProducts(results.data.Search);
+    };
+
+    useEffect(() => {
+        loadingProducts();
+    }, []);
+
+    return(
+        <>
+            <h1>Produtos</h1>
+            <ul>
+                {products.map(p => {
+                    return(
+                        <li>{p.Title}</li>
+                    );
+                })}
+            </ul>
+        </>
+    );
+}
+
+export default Produtos;
